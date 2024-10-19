69cc6fe2b9b629c83fc6f66681605b1ba6a81fda
Sarah6432 <sarah.s.lima973@gmail.com>
2024-10-19 12:46:12 -0300

    feat: colocando icone

--- graficos.js
+++ graficos.js
@@ -142,7 +142,7 @@ const dataHorizontalBar = {
     labels: ['Clientes', 'Empresários', 'Microempresários', 'Vendedores', 'Comerciantes', 'Gerentes'],
     datasets: [{
         label: 'Total',
-        data: [12, 19, 3, 5, 2, 3],
+        data: [100, 200, 320, 50, 20, 30],
         backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
             'rgba(54, 162, 235, 0.2)',
